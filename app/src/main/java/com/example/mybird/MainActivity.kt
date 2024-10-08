1af2fbb232793bf218d7cd7d165f54c3dd09c1ca
tranvietan2001 <tranvietan2001@gmail.com>
2024-10-08 20:30:44 +0700

    add image

--- app/src/main/java/com/example/mybird/MainActivity.kt
+++ app/src/main/java/com/example/mybird/MainActivity.kt
@@ -13,11 +13,5 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
