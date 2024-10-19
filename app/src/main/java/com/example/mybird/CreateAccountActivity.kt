e518ae20f6ae9f3ae6517870c2e8322a7f111dfd
tranvietan2001 <tranvietan2001@gmail.com>
2024-10-19 21:57:46 +0700

    func create Account ok

--- app/src/main/java/com/example/mybird/CreateAccountActivity.kt
+++ app/src/main/java/com/example/mybird/CreateAccountActivity.kt
@@ -4,17 +4,63 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
+
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var emailTxt: EditText
+    private lateinit var passwardTxt: EditText
+    private lateinit var nameAccTxt: EditText
+    private lateinit var createAccBtn: Button
+    private lateinit var failCreateTxt: TextView
+
+    private lateinit var firebaseManager: FirebaseManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge() //ẩn phần viền trên
         setContentView(R.layout.create_account_activity_main)
 
+        // Khởi tạo FirebaseManager
+        firebaseManager = FirebaseManager()
+        firebaseManager.initFirebase()
+
+        emailTxt = findViewById(R.id.emailAccTxt)
+        passwardTxt = findViewById(R.id.passwordAccTxt)
+        nameAccTxt = findViewById(R.id.nameAccTxt)
+        createAccBtn = findViewById(R.id.createAccBtn)
+        failCreateTxt = findViewById(R.id.failCreateTxt)
+
+        failCreateTxt.visibility = View.VISIBLE
+
+        createAccBtn.setOnClickListener {
+            val email = emailTxt.text
+            val password = passwardTxt.text
+            val name = nameAccTxt.text
+
+            val isCheckEmail = isValidEmail(email.toString())
+
+            Toast.makeText(this, "$isCheckEmail", Toast.LENGTH_SHORT).show()
+
+
+            if (email.isNotEmpty() && password.isNotEmpty() && isCheckEmail && password.toString().length >=6) {
+                // Gọi hàm tạo tài khoản trong coroutine
+                lifecycleScope.launch {
+                    /*val result = */firebaseManager.createAccount(email.toString(), password.toString())
+//                    Toast.makeText(this@CreateAccountActivity, result, Toast.LENGTH_SHORT).show()
+                }
+            } else {
+//                Toast.makeText(this, "Vui lòng nhập email và mật khẩu", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
@@ -30,5 +76,11 @@ class CreateAccountActivity : AppCompatActivity() {
         if (hasFocus) {
             hideSystemUI() // Đảm bảo chế độ toàn màn hình khi có tiêu điểm
         }
+    }
+
+    fun isValidEmail(email: String): Boolean {
+        // Biểu thức chính quy cho địa chỉ email
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+        return Regex(emailRegex).matches(email.trim())
     }
 }
--- app/src/main/java/com/example/mybird/LoginActivity.kt
+++ app/src/main/java/com/example/mybird/LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.example.mybird
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var accountNameTxt:EditText
     private lateinit var passwordTxt:EditText
     private lateinit var loginBtn:Button
+    private lateinit var failLoginTxt: TextView
+    private lateinit var createAccountTxt: TextView
+    private lateinit var forgotPassTxt: TextView
 
     private var accountName = ""
     private var password = ""
@@ -23,19 +29,39 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge() //ẩn phần viền trên
         setContentView(R.layout.login_activity_main)
 
-        accountNameTxt = findViewById(R.id.accountTxt)
-        passwordTxt = findViewById(R.id.passwordTxt)
-        loginBtn = findViewById(R.id.loginBtn)
+        accountNameTxt =    findViewById(R.id.accountTxt)
+        passwordTxt =       findViewById(R.id.passwordTxt)
+        loginBtn =          findViewById(R.id.loginBtn)
+        failLoginTxt =      findViewById(R.id.failLoginTxt)
+        createAccountTxt =  findViewById(R.id.signAccountTxt)
+        forgotPassTxt =     findViewById(R.id.forgotPassTxt)
 
-        loginBtn?.setOnClickListener {
+        failLoginTxt.visibility = View.GONE
+
+        loginBtn.setOnClickListener {
             accountName = accountNameTxt.text.toString()
             password = passwordTxt.text.toString()
             if((accountName == "" && password == "") || accountName == "" || password == ""){
+                failLoginTxt.visibility = View.VISIBLE
                 Toast.makeText(this, "ERROR: =========", Toast.LENGTH_LONG).show()
             }
             else {
+                failLoginTxt.visibility = View.GONE
                 Toast.makeText(this, "$accountName---$password", Toast.LENGTH_LONG).show()
+                val changeUi = Intent(this,PlayGameActivity::class.java)
+                startActivity(changeUi)
             }
+
+        }
+
+        createAccountTxt.setOnClickListener {
+            val changeUi = Intent(this,CreateAccountActivity::class.java)
+            startActivity(changeUi)
+        }
+
+        forgotPassTxt.setOnClickListener {
+            val changeUi = Intent(this,ForgotPasswordActivity::class.java)
+            startActivity(changeUi)
         }
 
     }
